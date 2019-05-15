061f998fb950061346db0943c943dcb13edcbb5e
mesher.x <mesher.x@icloud.com>
2019-05-15 21:48:47 +0300

    change import order and variable names

--- test/test_requeueing.py
+++ test/test_requeueing.py
@@ -1,64 +1,64 @@
-import aioredis
-import aioredisqueue
+import timeit
 import asyncio
 import pytest
-import timeit
+import aioredis
+import aioredisqueue
 
 @pytest.mark.asyncio
 async def test_requeue_method():
-    r = await aioredis.create_redis(('localhost', 6379), db=0)
-    q = aioredisqueue.queue.Queue(r)
+    redis = await aioredis.create_redis(('localhost', 6379), db=0)
+    queue = aioredisqueue.queue.Queue(redis)
 
-    await q.put(b'payload')
-    await q.get()
-    main_queue_len = await r.llen(q._keys['queue'])
-    ack_len = await r.hlen(q._keys['ack'])
+    await queue.put(b'payload')
+    await queue.get()
+    main_queue_len = await redis.llen(queue._keys['queue'])
+    ack_len = await redis.hlen(queue._keys['ack'])
     assert ack_len >= 1
-    await r.delete(q._keys['last_requeue'])
+    await redis.delete(queue._keys['last_requeue'])
     now = int(timeit.default_timer() * 1000)
-    results = await q.requeue()
+    results = await queue.requeue()
     assert results[0] == b'ok'
     assert len(results[1]) == ack_len
     assert len(results[3]) == ack_len
     assert results[2][-1] == main_queue_len + ack_len
-    assert int(await r.get(q._keys['last_requeue'])) == now
+    assert int(await redis.get(queue._keys['last_requeue'])) == now
 
-    r.close()
-    await r.wait_closed()
+    redis.close()
+    await redis.wait_closed()
 
 
 @pytest.mark.asyncio
 async def test_too_early():
-    r = await aioredis.create_redis(('localhost', 6379), db=0)
-    q = aioredisqueue.queue.Queue(r)
+    redis = await aioredis.create_redis(('localhost', 6379), db=0)
+    queue = aioredisqueue.queue.Queue(redis)
 
     now = int(timeit.default_timer() * 1000)
-    await r.set(q._keys['last_requeue'], now)
-    results = await q.requeue()
+    await redis.set(queue._keys['last_requeue'], now)
+    results = await queue.requeue()
     assert results[0] == b'error'
 
-    r.close()
-    await r.wait_closed()
+    redis.close()
+    await redis.wait_closed()
 
 
 @pytest.mark.asyncio
 async def test_in_background():
-    r = await aioredis.create_redis(('localhost', 6379), db=0)
+    redis = await aioredis.create_redis(('localhost', 6379), db=0)
     requeue_interval = 100
     eps = 0.01
-    q = aioredisqueue.queue.Queue(r, requeue_interval=requeue_interval)
+    queue = aioredisqueue.queue.Queue(redis, requeue_interval=requeue_interval)
 
-    await r.delete(q._keys['last_requeue'])
+    await redis.delete(queue._keys['last_requeue'])
 
-    await q.put(b'payload')
+    await queue.put(b'payload')
     await asyncio.sleep(eps)
-    task = await q.get()
+    task = await queue.get()
 
     await asyncio.sleep(requeue_interval / 1000)
-    assert await r.hget(q._keys['ack'], task.id) is not None
+    assert await redis.hget(queue._keys['ack'], task.id) is not None
 
     await asyncio.sleep(requeue_interval / 1000)
-    assert await r.hget(q._keys['ack'], task.id) is None
+    assert await redis.hget(queue._keys['ack'], task.id) is None
 
-    r.close()
-    await r.wait_closed()
+    redis.close()
+    await redis.wait_closed()
